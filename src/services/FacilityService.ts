3f962a47a4b69058b0f9a45ee0bb4b7aa69721ac
manjilthapa <manjilthapa@gmail.com>
2021-01-11 13:15:56 +0100

    Update apis and added insert facility

--- src/services/FacilityService.ts
+++ src/services/FacilityService.ts
@@ -1,14 +1,27 @@
 const KEYS = {
-    facilities: "facilities",
-  };
-  export function getAllFacilities() {
-    if (localStorage.getItem(KEYS.facilities) == null)
-      localStorage.setItem(KEYS.facilities, JSON.stringify([]));
-    return myPromise.then((value) => value);
+  facilities: "facilities",
+};
+export const getAllFacilities = async () => {
+  const facilities = JSON.parse(localStorage.getItem(KEYS.facilities) || "[]");
+  await responseApiPromise(facilities);
+  if (!facilities) {
+    return [];
   }
-  
-  const myPromise = new Promise((resolve, reject) => {
+  return facilities;
+};
+
+export const addFacility = async (data: any) => {
+  let facilites = await getAllFacilities(); //localStorage.getItem(KEYS.facilities);
+  if (facilites) {
+    facilites.push(data);
+    localStorage.setItem(KEYS.facilities, JSON.stringify(facilites));
+  }
+};
+
+const responseApiPromise = (data: any) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(localStorage.getItem(KEYS.facilities));
+      resolve(data);
     }, 1000);
   });
+};
