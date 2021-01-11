a8ca7db472b70cdc7c7165994f8894b9074b3bc9
Ali Fateminia <aif@trackman.com>
2021-01-11 12:19:21 +0100

    feat: added new apis

---
+++ src/services/FacilityService.ts
@@ -0,0 +1,14 @@
+const KEYS = {
+    facilities: "facilities",
+  };
+  export function getAllFacilities() {
+    if (localStorage.getItem(KEYS.facilities) == null)
+      localStorage.setItem(KEYS.facilities, JSON.stringify([]));
+    return myPromise.then((value) => value);
+  }
+  
+  const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(localStorage.getItem(KEYS.facilities));
+    }, 1000);
+  });
